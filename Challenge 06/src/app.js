15242eb1a6b2a88ccc1c90efeb9573a2210eb4a7
marraui <camilomarraui@gmail.com>
2019-10-06 16:04:44 -0500

    Added cors handling

--- Challenge 06/src/app.js
+++ Challenge 06/src/app.js
@@ -4,6 +4,7 @@ import { login, register, loginRequired } from './controllers/login';
 import { dbConnection } from './models/dbmanager';
 import { verifyWebToken} from './middleware/verify_webtoken';
 import * as book from './routes/book';
+import cors from 'cors';
 
 export class App {
     constructor () {
@@ -13,6 +14,19 @@ export class App {
             console.log('Connected succesfully to database');
             this.app.use(bodyParser.urlencoded({extended: true}));
             this.app.use(bodyParser.json());
+
+            this.app.use(cors());
+
+            /**
+             * * Enable corse for all origins
+             */
+            this.app.use((req, res, next) => {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+                res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+                res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+                next();
+            });
     
             this.app.post('/login', login);
             this.app.post('/register', register);

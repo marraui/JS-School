e96ca1363ee529807e305aaf4b3182e2c1060b2d
marraui <camilomarraui@gmail.com>
2019-10-13 01:22:35 -0500

    Added time interval query parameters

--- Challenge 07/back-end/src/controllers/book/lend_book.js
+++ Challenge 07/back-end/src/controllers/book/lend_book.js
@@ -5,6 +5,27 @@ import { LentInfo } from '../../models/lent';
 export async function lendBook(req, res, next) {
     const user = req.user;
     const id = req.params.id;
+    const {
+        reservationTime,
+        returnTime,
+    } = req.query;
+
+    if (!reservationTime) {
+        console.log(`Lend book -> No reservation time provided`);
+        res.status(HttpStatus.BAD_REQUEST).send({message: 'No reservation time provided'});
+        return
+    }
+    if (!returnTime) {
+        console.log(`Lend book -> No return time provided`);
+        res.status(HttpStatus.BAD_REQUEST).send({ message: 'No return time provided' });
+        return;
+    }
+
+    if (Number(reservationTime) > Number(returnTime)) {
+        console.log(`Lend book -> The time of the reservation cannot be greater than the return time`);
+        res.status(HttpStatus.BAD_REQUEST).send({ message: 'The time of the reservation cannot be greater than the return time'});
+        return;
+    }
     let error;
 
     if (!id) {
@@ -33,22 +54,24 @@ export async function lendBook(req, res, next) {
         return;
     }
 
-    const lentInfo = await dbConnection.getLatestsLentInfo(book.id).catch(err => {
+    const isBookAvailable = await dbConnection.isBookAvailable(reservationTime, returnTime).catch(err => {
         error = err
         console.log(`Lend book -> Error finding lent information, error: ${err.message}`);
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: 'Error finding lent information' });
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({message: 'Error finding lent information'});
     });
     if (error) return;
 
-    if (lentInfo && !lentInfo.returnedTime) {
+    if (!isBookAvailable) {
         console.log(`Lend book -> Book is not available`);
-        res.status(HttpStatus.NO_CONTENT).send({ message: 'Book is already lent to a user' });
+        res.status(HttpStatus.NO_CONTENT).send();
         return;
     }
 
     const updatedBook = await dbConnection.insertLentInfo(new LentInfo({
         book: book,
-        user: user
+        user: user,
+        lentTime: reservationTime,
+        returnedTime: returnTime,
     })).catch(err => {
         error = err
         console.log(`Lend book -> Error insert lend information, Error: ${err.message}`);

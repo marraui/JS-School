66bcbc86c48f9d536dfac7372ebe701cb8a4180d
marraui <camilomarraui@gmail.com>
2019-10-18 18:16:55 -0500

    Added 404 page

--- Challenge 08/front-end/src/components/routing/Routing.jsx
+++ Challenge 08/front-end/src/components/routing/Routing.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch, Redirect,
+} from 'react-router-dom';
 import Login from '../login/Login';
 import App from '../app/App';
+import NotFound from '../notfound/NotFound';
 
 export default function Routing() {
   return (
@@ -9,6 +14,8 @@ export default function Routing() {
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/login" component={Login} />
+        <Route path="/404" component={NotFound} />
+        <Redirect to="/404" />
       </Switch>
     </Router>
   );

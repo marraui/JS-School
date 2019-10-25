53f598d05865bd122732a81fa1b36dd2e16eed97
marraui <camilomarraui@gmail.com>
2019-10-25 14:21:54 -0500

    Changed sass for styled components in Header

--- Challenge 09/front-end/src/components/header/Header.jsx
+++ Challenge 09/front-end/src/components/header/Header.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Header.scss';
 import logo from '../../assets/images/logo2.svg';
 import logoText from '../../assets/images/logo-jobsity-text.svg';
 import userImage from '../../assets/images/user_image.jpg';
 import Search from '../search/Search';
+import {
+  HeaderContainer,
+  Logo,
+  UserProfile,
+} from './Layout';
 
 export default function Header(props) {
   const { onSearch } = props;
   return (
-    <div className="header">
+    <HeaderContainer>
       <Search onSearch={onSearch} />
 
 
-      <div className="logo">
+      <Logo className="logo">
         <img src={logo} alt="Jobsity logo" className="jobsity-logo" />
         <img src={logoText} alt="Jobsity text logo" className="jobsity-text-logo" />
-      </div>
+      </Logo>
 
 
-      <div className="user">
+      <UserProfile>
         <div className="user-border-wrapper">
           <div className="user-name-dropdown">
             <div className="user-name-text">Jakob Treml</div>
@@ -31,8 +35,8 @@ export default function Header(props) {
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </UserProfile>
+    </HeaderContainer>
   );
 }
 

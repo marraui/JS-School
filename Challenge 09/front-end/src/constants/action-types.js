e243d5d858b4efbcce9f7955e476197033db5caf
marraui <camilomarraui@gmail.com>
2019-10-25 10:17:28 -0500

    Added constants for all actions types

---
+++ Challenge 09/front-end/src/constants/action-types.js
@@ -0,0 +1,21 @@
+export const SEARCH_BOOK = 'SEARCH_BOOK';
+
+export const SELECT_CITY = 'SELECT_CITY';
+
+export const SELECT_FORMAT = 'SELECT_FORMAT';
+
+export const UNSELECT_CITY = 'UNSELECT_CITY';
+
+export const UNSELECT_FORMAT = 'UNSELECT_FORMAT';
+
+export const SELECT_PAGE = 'SELECT_PAGE';
+
+export const BOOKS_FETCHED = 'BOOKS_FETCHED';
+
+export const FETCHING_BOOKS = 'FETCHING_BOOKS';
+
+export const TOTAL_OF_BOOKS = 'TOTAL_OF_BOOKS';
+
+export const RES_PER_PAGE = 'RES_PER_PAGE';
+
+export const LOG_IN = 'LOG_IN';

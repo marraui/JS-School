5da9e67131150b59c615e18c4964d55f6d7532d3
marraui <camilomarraui@gmail.com>
2019-11-01 10:53:47 -0500

    Changed default state for page from 1 to 0

--- Challenge 10/front-end/src/reducers/page.js
+++ Challenge 10/front-end/src/reducers/page.js
@@ -1,6 +1,6 @@
 import { SELECT_PAGE } from '../constants/action-types';
 
-export default function page(state = 1, action) {
+export default function page(state = 0, action) {
   if (action.type === SELECT_PAGE) {
     return action.payload ? action.payload : state;
   }

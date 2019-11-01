54f6e7d61301764814fdd00949fa57e958431194
marraui <camilomarraui@gmail.com>
2019-11-01 10:46:04 -0500

    Added availability attribute to getBooks response

--- Challenge 10/back-end/src/controllers/book/get_books.js
+++ Challenge 10/back-end/src/controllers/book/get_books.js
@@ -24,6 +24,24 @@ export async function getBooks(req, res, next) {
         console.log(`Get books -> Error finding books`);
         res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: 'Error finding books' });
     }
+
+    const availability = await Promise.all(result.books.map((book) => {
+        if (book.format === 'Digital') return false;
+        const begin = new Date();
+        begin.setHours(0, 0, 0, 0);
+        const end = new Date();
+        end.setHours(23, 59, 59, 999);
+        return dbConnection.isBookAvailable(book.id, begin.getTime(), end.getTime());
+    })).catch((err) => {
+        error = err;
+        console.log(`Get book -> Error getting the availability of books, error: ${err.message}`);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({message: 'Error getting books availability'});
+    });
+    if (error) return;
+    
+    for (let i = 0; i < result.books.length; i++) {
+        result.books[i].available = availability[i];
+    }
     console.log(`Get books -> books retrieved successfully`);
     res.status(HttpStatus.OK).json(result);
 }

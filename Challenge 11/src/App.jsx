abe95a71b928dc4f13098bcb2dd1c568885055fe
marraui <camilomarraui@gmail.com>
2019-11-05 17:24:00 -0500

    Added redux and theme providers

--- Challenge 11/src/App.jsx
+++ Challenge 11/src/App.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Provider } from 'react-redux';
+import './App.css';
+import { ThemeProvider } from 'styled-components';
 import store from './store/index';
+import HomePage from './components/HomePage/HomePage';
+import theme from './styles/theme';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider theme={theme}>
+        <HomePage />
+      </ThemeProvider>
     </Provider>
   );
 }

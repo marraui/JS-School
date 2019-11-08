90424a968082a48cf7bde75450443bd2c915c5f5
marraui <camilomarraui@gmail.com>
2019-11-07 22:29:24 -0500

    Added id and title for default selectedInterval

--- Challenge 11/src/reducers/selected-interval.js
+++ Challenge 11/src/reducers/selected-interval.js
@@ -1,6 +1,11 @@
 import { SELECT_INTERVAL } from '../constants/action-types';
 
-export default function selectedInterval(state = { start: 0, end: null }, action) {
+export default function selectedInterval(state = {
+  start: 0,
+  end: null,
+  id: 0,
+  title: 'Full clip',
+}, action) {
   if (action.type === SELECT_INTERVAL) {
     return action.payload;
   }

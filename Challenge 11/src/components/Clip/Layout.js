7f5a0b3897581f6f0c9275ff35a51a7d50063203
marraui <camilomarraui@gmail.com>
2019-11-07 22:27:57 -0500

    Added color when selected

--- Challenge 11/src/components/Clip/Layout.js
+++ Challenge 11/src/components/Clip/Layout.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const ClipWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  background-color: ${(props) => props.theme.lightPrimary};
+  background-color: ${(props) => (props.selected ? props.theme.secondary : props.theme.lightPrimary)};
   border-radius: 1rem;
   padding: 1rem;
-  box-shadow: 0rem 0rem 0.8rem;
+  box-shadow: 0rem 0rem 0.1rem;
+  cursor: pointer;
   &:hover {
     background-color: ${(props) => props.theme.darkSecondary};
   }
@@ -15,6 +16,7 @@ export const ClipWrapper = styled.div`
 export const InvisibleInput = styled.input`
   background-color: inherit;
   border-style: unset;
+  border-bottom: 1px solid ${(props) => props.theme.lightSecondaryText};
   font-family: 'Pluto Sans Cond Regular';
   font-size: 1rem;
 `;

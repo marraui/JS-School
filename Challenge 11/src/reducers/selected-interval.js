f35f61f45d21a579908fe15b9c5aa0093b5b00eb
marraui <camilomarraui@gmail.com>
2019-11-12 12:48:23 -0500

    Fixed update selectd interval when update interval action is dispatched

--- Challenge 11/src/reducers/selected-interval.js
+++ Challenge 11/src/reducers/selected-interval.js
@@ -1,13 +1,20 @@
-import { SELECT_INTERVAL } from '../constants/action-types';
+import { SELECT_INTERVAL, UPDATE_INTERVAL } from '../constants/action-types';
 
 export default function selectedInterval(state = {
   start: 0,
   end: null,
   id: 0,
   title: 'Full clip',
+  tags: [],
 }, action) {
   if (action.type === SELECT_INTERVAL) {
     return action.payload;
+  }
+
+  if (action.type === UPDATE_INTERVAL) {
+    if (action.payload.id === state.id) {
+      return action.payload;
+    }
   }
 
   return state;

37482739adba307e78f1c059cf7c3829bed43ace
marraui <camilomarraui@gmail.com>
2019-11-14 20:51:30 -0500

    Fix layout of tag display

--- Challenge 11/src/components/TagDisplay/Layout.js
+++ Challenge 11/src/components/TagDisplay/Layout.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr;
+  row-gap: 0.8rem;
 `;
 
 export const SubmitButton = styled.input`

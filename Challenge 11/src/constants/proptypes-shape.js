c23184d423310ed52909282521699620c464df93
marraui <camilomarraui@gmail.com>
2019-11-15 14:22:12 -0500

    Added shape of interval as constant

---
+++ Challenge 11/src/constants/proptypes-shape.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line import/prefer-default-export
+export const intervalPropType = PropTypes.shape({
+  id: PropTypes.number,
+  start: PropTypes.number,
+  end: PropTypes.number,
+  color: PropTypes.string,
+});

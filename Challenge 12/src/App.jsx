57184e6c639b74686dc398dbaf26f35f84b55be9
marraui <camilomarraui@gmail.com>
2019-11-21 14:59:50 -0500

    Added a theme

--- Challenge 12/src/App.jsx
+++ Challenge 12/src/App.jsx
@@ -3,11 +3,12 @@ import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import store from './store';
 import Routing from './containers/Routing';
+import theme from './styles/theme';
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={{}}>
+      <ThemeProvider theme={theme}>
         <Routing />
       </ThemeProvider>
     </Provider>
---
+++ Challenge 12/src/styles/theme.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+export default {
+  primaryColor: '#FFFFFF',
+  secondaryColor: '#5ECC5C',
+  lightSecondaryColor: '#A6D9A5',
+  primaryTextColor: '#000000',
+  lightPrimaryTextColor: '#595959',
+  redFilter: 'rgba(255, 0, 0, 0.5)',
+};
+
+export const themePropTypes = {
+  primaryColor: PropTypes.string,
+  secondaryColor: PropTypes.string,
+  lightSecondaryColor: PropTypes.string,
+  primaryTextColor: PropTypes.string,
+  lightPrimaryTextColor: PropTypes.string,
+  redFilter: PropTypes.string,
+};

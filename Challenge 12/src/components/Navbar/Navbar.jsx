12de16b11393c3bb70031e27f9c8c02e4673ef8e
marraui <camilomarraui@gmail.com>
2019-11-21 16:31:36 -0500

    Added functionality to Navbar

--- Challenge 12/src/components/Navbar/Navbar.jsx
+++ Challenge 12/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { tabsPropType } from '../../constants/redux-types';
 import {
   Container,
   NavbarLink,
+  BurgerButton,
+  BurgerButtonContainer,
+  TabsContainer,
 } from './Layout';
 
 export default function Navbar({ tabs }) {
+  const { pathname: selectedTab } = useLocation();
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <Container>
-      {tabs.map((tab) => (
-        <NavbarLink key={tab}>
-          {tab}
-        </NavbarLink>
-      ))}
+      <BurgerButtonContainer>
+        <BurgerButton onClick={() => setMenuOpened(!menuOpened)}>
+          <i className="fa fa-bars" />
+        </BurgerButton>
+      </BurgerButtonContainer>
+      <TabsContainer open={menuOpened}>
+        {tabs.map((tab) => (
+          <NavbarLink key={tab} to={tab} selected={`/${tab}` === selectedTab}>
+            {tab}
+          </NavbarLink>
+        ))}
+      </TabsContainer>
     </Container>
   );
 }

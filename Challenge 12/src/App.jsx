ac04fc8c0ebcf1a5dc2ececfcfd3f2ec85093f6f
marraui <camilomarraui@gmail.com>
2019-11-21 14:10:55 -0500

    Added redux store

--- Challenge 12/src/App.jsx
+++ Challenge 12/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import store from './store';
+import Routing from './containers/Routing';
 
 function App() {
   return (
-    <div>Hello world!</div>
+    <Provider store={store}>
+      <ThemeProvider theme={{}}>
+        <Routing />
+      </ThemeProvider>
+    </Provider>
   );
 }
 

41bc40fde9d49bc113e3954e7b91f17297d55cb5
marraui <camilomarraui@gmail.com>
2019-11-28 09:23:09 -0500

    Added lightPrimaryColor and error color

--- Challenge 12/src/styles/theme.js
+++ Challenge 12/src/styles/theme.js
@@ -2,11 +2,13 @@ import PropTypes from 'prop-types';
 
 export default {
   primaryColor: '#FFFFFF',
+  lightPrimaryColor: '#E3E3E3',
   secondaryColor: '#5ECC5C',
   lightSecondaryColor: '#A6D9A5',
   primaryTextColor: '#000000',
   lightPrimaryTextColor: '#595959',
   redFilter: 'rgba(255, 0, 0, 0.5)',
+  error: '#FF0000',
 };
 
 export const themePropTypes = {

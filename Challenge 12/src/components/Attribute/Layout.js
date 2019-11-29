7d907333afc4a46cbf1727371da655819fb041fd
marraui <camilomarraui@gmail.com>
2019-11-28 21:41:42 -0500

    Added delete and toggle buttons

--- Challenge 12/src/components/Attribute/Layout.js
+++ Challenge 12/src/components/Attribute/Layout.js
@@ -3,10 +3,31 @@ import styled from 'styled-components';
 // eslint-disable-next-line import/prefer-default-export
 export const Wrapper = styled.div`
   display: grid;
+  position: relative;
   grid-template-columns: 1fr 2fr;
   column-gap: 16px;
   row-gap: 8px;
   padding: 35px 25px;
   box-shadow: 0px -3px ${({ theme }) => theme.lightPrimaryColor};
   background-color: ${({ theme }) => theme.primaryColor};
+`;
+
+export const DeleteIcon = styled.i.attrs(() => ({ className: 'fa fa-trash' }))`
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.lightPrimaryTextColor};
+  cursor: pointer;
+`;
+
+export const ToggleIcon = styled.i.attrs(({ opened }) => ({
+  className: `fa fa-angle-double-${(opened && 'up') || 'down'}`,
+}))`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.lightPrimaryTextColor};
+  cursor: pointer;
 `;

b1682f45c967396042e777ff24982b7859c33ac0
marraui <camilomarraui@gmail.com>
2019-11-29 10:57:23 -0500

    Fixed export action creators

---
+++ Challenge 12/src/actions/index.js
@@ -0,0 +1,3 @@
+export { default as addAttribute } from './add-attribute';
+export { default as removeAttribute } from './remove-attribute';
+export { default as updateAttribute } from './update-attribute';

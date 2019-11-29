4fb887e96ef1b5e8e3231bed6caac5e00ed12f9c
marraui <camilomarraui@gmail.com>
2019-11-29 10:59:03 -0500

    Connected MainDisplay to redux

--- Challenge 12/src/containers/MainDisplay/index.js
+++ Challenge 12/src/containers/MainDisplay/index.js
@@ -1,3 +1,11 @@
+import { connect } from 'react-redux';
 import MainDisplay from './MainDisplay';
+import { addAttribute } from '../../actions';
 
-export default MainDisplay;
+const mapStateToProps = (state) => ({ attributes: state.attributes });
+
+const mapDispatchToProps = (dispatch) => ({
+  addAttribute: (attribute) => dispatch(addAttribute(attribute)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainDisplay);

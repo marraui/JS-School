c2eeef61eb106b0ee074a4d1ffb621e2b2e8e127
marraui <camilomarraui@gmail.com>
2019-11-29 10:57:43 -0500

    Added default values for new attribute

--- Challenge 12/src/actions/add-attribute.js
+++ Challenge 12/src/actions/add-attribute.js
@@ -1,8 +1,9 @@
 import { ADD_ATTRIBUTE } from '../constants/action-types';
+import defaultAttribute from '../constants/default-attribute';
 
 export default function addAttribute(payload) {
   return {
     type: ADD_ATTRIBUTE,
-    payload,
+    payload: { ...defaultAttribute, ...payload, id: new Date().getTime() },
   };
 }

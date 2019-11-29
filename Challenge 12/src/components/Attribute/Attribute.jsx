ec5110cc8d3203057eb2d6ca0e86c60974716d53
marraui <camilomarraui@gmail.com>
2019-11-28 21:41:26 -0500

    Added object representing the structure of the view

--- Challenge 12/src/components/Attribute/Attribute.jsx
+++ Challenge 12/src/components/Attribute/Attribute.jsx
@@ -1,28 +1,153 @@
-import React from 'react';
+/* eslint-disable react/no-unused-prop-types */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import FormInput from '../FormInput';
 import {
   Wrapper,
+  DeleteIcon,
+  ToggleIcon,
 } from './Layout';
 
-export default function Attribute({
-  name,
-  description,
-  deviceResourceType,
-  defaultValue,
-  dataType,
-  format,
-  noneFields,
-  numberFields,
-}) {
+const dataTypeOptions = ['STRING', 'NONE'];
+const formatOptions = ['NONE', 'NUMBER', 'BOOLEAN', 'DATE-TIME', 'CDATA', 'URI'];
+
+function getFields(fields) {
+  const stack = [...fields];
+  const res = [];
+  stack.reverse();
+  while (stack.length > 0) {
+    const field = stack.pop();
+    field.accessor = typeof field.accessor === 'string' ? [field.accessor] : field.accessor;
+    if (field.fieldType === 'group') {
+      stack.push(...field.fields.map((child) => ({
+        ...child,
+        exclusiveTo: field.exclusiveTo,
+        accessor: [...field.accessor, child.accessor],
+      })));
+    } else res.push(field);
+  }
+  return res;
+}
+
+const inputs = [
+  {
+    name: 'Name',
+    accessor: 'name',
+    placeholder: 'Enter a name',
+  },
+  {
+    name: 'Description',
+    accessor: 'description',
+    placeholder: 'Enter a description of your new attribute',
+  },
+  {
+    name: 'Device Resource Type',
+    accessor: 'deviceResourceType',
+    type: 'select',
+    options: [],
+    concealable: true,
+  },
+  {
+    name: 'Default Value',
+    accessor: 'defaultValue',
+    placeholder: 'Enter a default value',
+    concealable: true,
+  },
+  {
+    name: 'Data Type',
+    accessor: 'dataType',
+    type: 'select',
+    options: dataTypeOptions,
+    concealable: true,
+  },
+  {
+    name: 'Format',
+    accessor: 'format',
+    type: 'select',
+    options: formatOptions,
+    concealable: true,
+  },
+  {
+    exclusiveTo: 'NONE',
+    accessor: 'noneFields',
+    fieldType: 'group',
+    fields: [
+      {
+        name: 'Enumerations',
+        accessor: 'enumerations',
+        placeholder: 'Enumerations',
+        concealable: true,
+      },
+    ],
+  },
+  {
+    exclusiveTo: 'NUMBER',
+    accessor: 'numberFields',
+    fieldType: 'group',
+    fields: [
+      {
+        fieldType: 'group',
+        accessor: 'range',
+        fields: [
+          {
+            name: 'Range Min',
+            accessor: 'min',
+            placeholder: 'Range min',
+            concealable: true,
+          },
+          {
+            name: 'Range Max',
+            accessor: 'max',
+            placeholder: 'Range max',
+            concealable: true,
+          },
+        ],
+      },
+      {
+        name: 'Unit of Measurement',
+        accessor: 'unitOfMeasurement',
+        placeholder: 'UoM (eg. mm)',
+        concealable: true,
+      },
+      {
+        name: 'Precision',
+        accessor: 'precision',
+        placeholder: 'Precision (eg. 0.5)',
+        concealable: true,
+      },
+      {
+        name: 'Accuracy',
+        accessor: 'accuracy',
+        placeholder: 'Accuracy (eg. 0.5)',
+        concealable: true,
+      },
+    ],
+  },
+];
+
+function fieldToNode(field, values, opened) {
   return (
-    <Wrapper>
-      <FormInput name="name" value={name} />
-      <FormInput name="description" value={description} />
-      <FormInput name="deviceResourceType" value={deviceResourceType} />
-      <FormInput name="defaultValue" value={defaultValue} />
-      <FormInput name="dataType" value={dataType} />
-      <FormInput name="format" value={format} />
+    !field.exclusiveTo || field.exclusiveTo === values.format ? (
+      <FormInput
+        name={field.name}
+        value={field.accessor.reduce((obj, acc) => obj[acc], values)}
+        placeholder={field.placeholder}
+        type={field.type}
+        options={field.options}
+        hide={field.concealable && !opened}
+      />
+    ) : null);
+}
+
+export default function Attribute(props) {
+  const [opened, setOpened] = useState(true);
+  const inputFields = getFields(inputs);
+  return (
+    <Wrapper hide={!opened}>
+      <DeleteIcon />
+      <ToggleIcon opened={opened} onClick={() => setOpened(!opened)} />
+      {inputFields.map((field) => fieldToNode(field, props, opened))}
+
     </Wrapper>
   );
 }
@@ -47,6 +172,7 @@ Attribute.propTypes = {
     }),
     unitOfMeasurement: PropTypes.string,
     precision: PropTypes.number,
+    accuracy: PropTypes.number,
   }),
 };
 

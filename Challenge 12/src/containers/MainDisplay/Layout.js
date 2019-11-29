2ce47b50a9a6ee12fa7e8a4b3f660c83ecfb66de
marraui <camilomarraui@gmail.com>
2019-11-29 10:59:12 -0500

    Added add attribute button

--- Challenge 12/src/containers/MainDisplay/Layout.js
+++ Challenge 12/src/containers/MainDisplay/Layout.js
@@ -9,3 +9,26 @@ export const Wrapper = styled.div`
   border-width: 5px 2px 2px 2px;
   background-color: ${({ theme }) => theme.lightPrimaryColor};
 `;
+
+export const AddAttributeWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: ${({ theme }) => theme.lightPrimaryColor};
+  padding: 5px;
+`;
+
+export const AddAttributeIcon = styled.i.attrs(() => ({ className: 'fa fa-plus-circle' }))`
+  font-size: 1rem;
+  margin-right: 5px;
+  color: ${({ theme }) => theme.secondaryColor};
+  cursor: pointer;
+`;
+
+export const AddAttribute = styled.span`
+  font-size: 1rem;
+  color: ${({ theme }) => theme.secondaryColor};
+  font-weight: bold;
+  cursor: pointer;
+`;
--- Challenge 12/src/containers/MainDisplay/MainDisplay.jsx
+++ Challenge 12/src/containers/MainDisplay/MainDisplay.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { attributePropTypes } from '../../constants/redux-types';
 import FormInput from '../../components/Attribute';
 import {
   Wrapper,
+  AddAttribute,
+  AddAttributeIcon,
+  AddAttributeWrapper,
 } from './Layout';
 
-const sample = [1, 2, 3, 4];
-export default function MainDisplay() {
+export default function MainDisplay({ attributes, addAttribute }) {
+  const location = useLocation();
+  const tab = location.pathname.substring(1);
   return (
     <Wrapper>
-      {sample.map((attribute) => (
+      {attributes.filter((attribute) => attribute.tab === tab).map((attribute) => (
         <FormInput key={attribute} />
       ))}
+      <AddAttributeWrapper>
+        <AddAttributeIcon onClick={() => addAttribute({ tab })} />
+        <AddAttribute onClick={() => addAttribute({ tab })}>Add attribute</AddAttribute>
+      </AddAttributeWrapper>
     </Wrapper>
   );
 }
+
+MainDisplay.propTypes = {
+  addAttribute: PropTypes.func,
+  attributes: PropTypes.shape(attributePropTypes),
+};
+
+MainDisplay.defaultProps = {
+  addAttribute: () => null,
+  attributes: [],
+};
